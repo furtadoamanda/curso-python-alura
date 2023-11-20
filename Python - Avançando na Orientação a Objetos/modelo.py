674d7f0cab78832339143a4d6e2c328679ee06f6
Amanda Furtado <furtadodesouzaamanda@gmail.com>
2023-11-20 14:41:14 -0300

    Aula 4

--- Python - Avançando na Orientação a Objetos/modelo.py
+++ Python - Avançando na Orientação a Objetos/modelo.py
@@ -1,10 +1,10 @@
 class Programa:
     def __init__(self, nome, ano):
-        self._nome = nome.title() 
+        self._nome = nome.title() # Usando um _ só permite que os filhos acessem
         self.ano = ano
-        self._likes = 0 
+        self._likes = 0 # Valor inicial
 
-    @property  
+    @property  # É uma alternativa ao uso de getters e setters, para evitar ter que alterar todos os métodos do código que dependem da propriedade que se tornou privada
     def likes(self):
         return self._likes
 
@@ -22,10 +22,10 @@ class Programa:
     def __str__(self):
         return f'{self._nome} - {self.ano} - {self._likes} Likes'
 
-class Filme(Programa): 
+class Filme(Programa): # Os parênteses indicam qual é a "classe mãe", que faz com que as classes filhas herdem todas as informações
     def __init__(self, nome, ano, duracao):
-        super().__init__(nome, ano)
-        self.duracao = duracao
+        super().__init__(nome, ano) # Chama o inicializador da classe mãe (super classe), usando suas propriedades comuns
+        self.duracao = duracao # Propriedade específica da classe filha
 
     def __str__(self):
         return f'{self._nome} - {self.ano} - {self.duracao} min - {self._likes} Likes'
@@ -38,14 +38,39 @@ class Serie(Programa):
     def __str__(self):
         return f'{self._nome} - {self.ano} - {self.temporadas} temporadas - {self._likes} Likes'
 
+
+class Playlist:
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
+
+    @property
+    def listagem(self):
+        return self._programas
+
+    @property
+    def tamanho(self):
+        return len(self._programas)
+
+
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
-vingadores.dar_like()
-
 atlanta = Serie('atlanta', 2018, 2)
+tmep = Filme('todo mundo em pânico', 1999, 100)
+demolidor = Serie('demolidor', 2016, 2)
+
+vingadores.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+demolidor.dar_like()
+demolidor.dar_like()
 atlanta.dar_like()
 atlanta.dar_like()
 
-filmes_e_series = [vingadores, atlanta]
+filmes_e_series = [vingadores, atlanta, demolidor, tmep] # A lista tem uma ordem definida
+playlist_fim_de_semana = Playlist('Fim de Semana', filmes_e_series)
 
-for programa in filmes_e_series:
+print(f'Tamanho da playlist: {len(playlist_fim_de_semana.listagem)}')
+
+for programa in playlist_fim_de_semana.listagem:
     print(programa)
